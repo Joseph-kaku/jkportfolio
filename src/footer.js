18d36448037e2837a212345b74d0905ab8007146
josephkaku <samkaks18@gmail.com>
2024-01-02 16:01:53 -0700

    added a few more styles

--- src/footer.js
+++ src/footer.js
@@ -5,10 +5,11 @@ return (
     <footer>
         <p> Contact Me </p>
         <div class="contact">
-            <hr></hr>
-            <p><a href="#">Email</a> </p>
-            <p><a href="https://github.com/Joseph-kaku">GitHub</a> </p>
-            <p><a href="https://linkedin.com/in/joseph-kaku">LinkedIn</a> </p>
+            <ul>
+            <li><a href="#">Email</a> </li>
+            <li><a href="https://github.com/Joseph-kaku">GitHub</a></li>
+            <li><a href="https://linkedin.com/in/joseph-kaku">LinkedIn</a></li>
+            </ul>
         </div>
     </footer>
 );
